bce485e51c4bc6609bb48322d5507e7ba9513795
hongjiwon <wldnjsthsu1@gmail.com>
2025-06-04 15:23:28 +0900

    fix: change base path for Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,28 @@
+// import { defineConfig } from 'vite'
+// import vue from '@vitejs/plugin-vue'
+// import path from 'path' // ⬅️ path 모듈 꼭 import!
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   base: '/pupi/',
+//   plugins: [vue()],
+//   resolve: {
+//     alias: {
+//       '@': path.resolve(__dirname, './src') // ⬅️ 이 줄이 핵심!
+//     }
+//   }
+// })
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import path from 'path' // ⬅️ path 모듈 꼭 import!
+import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/pupi/',
+  base: '/',
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src') // ⬅️ 이 줄이 핵심!
+      '@': path.resolve(__dirname, './src')
     }
   }
 })
