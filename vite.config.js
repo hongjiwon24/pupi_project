f48c26dace0b371a351776ee54b4e64f8092385e
hongjiwon <wldnjsthsu1@gmail.com>
2025-06-30 17:09:45 +0900

    다시 커밋

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,3 @@
-// import { defineConfig } from 'vite'
-// import vue from '@vitejs/plugin-vue'
-// import path from 'path' // ⬅️ path 모듈 꼭 import!
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   base: '/pupi/',
-//   plugins: [vue()],
-//   resolve: {
-//     alias: {
-//       '@': path.resolve(__dirname, './src') // ⬅️ 이 줄이 핵심!
-//     }
-//   }
-// })
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
